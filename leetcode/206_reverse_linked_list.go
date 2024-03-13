ccbdeeb7b7ad5a2db7848a4fc358b0cc9fac7d96
panbao <panbao@miliantech.com>
2024-03-13 23:36:35 +0800

    反转链表

---
+++ leetcode/206_reverse_linked_list.go
@@ -0,0 +1,15 @@
+package leetcode
+
+//反转链表
+
+func reverseList(head *ListNode) *ListNode {
+    var pre *ListNode
+	curr := head
+	for curr != nil{
+		tmp := curr.Next
+		curr.Next = pre
+		pre = curr
+		curr = tmp
+	}
+	return pre
+}
---
+++ leetcode/demo_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func PrintNodeList(head *ListNode){
+	for head != nil{
+		fmt.Print(head.Val,",")
+		head = head.Next
+	}
+}
+
+func TestReverseList(t *testing.T){
+	head1 := &ListNode{Val: 1}
+	head2 := &ListNode{Val: 2}
+	head3 := &ListNode{Val: 3}
+	head1.Next = head2
+	head2.Next = head3
+	PrintNodeList(head1)
+	h1 := reverseList(head1)
+	PrintNodeList(h1)
+}
